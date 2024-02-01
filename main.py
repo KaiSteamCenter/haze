03c2c41c7f0e2a367a558540f6f35ebd809acded
Mekhi Mcfowler <mmcfowler@brooklynsteamcenter.org>
2024-02-01 10:53:49 -0500

    commiting everything

--- main.py
+++ main.py
@@ -1,13 +1,50 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
+import pymysql
+import pymysql.cursors
 
 app = Flask(__name__)
 
-@app.route('/')
+connection = pymysql.connect(
+    host="10.100.33.60",
+    user="mmcfowler",
+    password="220878185",
+    database="mmcfowler_haze",
+    charset="utf8mb4",
+    cursorclass=pymysql.cursors.DictCursor,
+)
+
+
+@app.route("/")
 def index():
-    return render_template('landing.html.jinja')
+    return render_template("landing.html.jinja")
 
 
+@app.route("/register", methods=["GET", "POST"])
+def register():
+    if request.method == "POST":
+        Username = request.form["username"]
+        Firstname = request.form["first_name"]
+        Lastname = request.form["last_name"]
+        DOB = request.form["dob"]
+        Password = request.form["password"]
+
+    try:
+        with connection.cursor() as cursor:
+            sql = "INSERT INTO User (Username, Firstname, Lastname, DOB, Password) VALUES (%s, %s, %s, %s, %s)"
+            cursor.execute(sql, (Username, Firstname, Lastname, DOB, Password))
+            connection.commit()
+            return redirect(url_for("/"))
+    except Exception as e:
+        # Handle the error, for example, print it
+        print(f"Error during registration: {e}")
+
+    return render_template("register.html.jinja")
 
 
-if __name__ == '__main__':
+@app.route("/signin")
+def signin():
+    return render_template("signin.html.jinja")
+
+
+if __name__ == "__main__":
     app.run(debug=True)
