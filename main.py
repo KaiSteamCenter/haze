bbf465f6402e1ec37ad1a04664051689fe0af24c
Mekhi Mcfowler <mmcfowler@brooklynsteamcenter.org>
2024-02-16 10:07:18 -0500

    MY EYES

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, redirect, url_for, g
 from pymysql.err import IntegrityError
 import pymysql
 import pymysql.cursors
@@ -18,42 +18,58 @@ class User:
         self.username = username
         self.id = id
         self.pfp = ProfilePic
-        
     def get_id(self):
         return str(self.id)
 
-connection = pymysql.connect(
-    host="10.100.33.60",
-    user="mmcfowler",
-    password="220878185",
-    database="mmcfowler_haze",
-    charset="utf8mb4",
-    cursorclass=pymysql.cursors.DictCursor,
-)
+
+
+def connect_db():
+    return pymysql.connect(
+        host="10.100.33.60",
+        user="mmcfowler",
+        password="220878185",
+        database="mmcfowler_haze",
+        cursorclass=pymysql.cursors.DictCursor,
+        autocommit=True
+    )
+
+def get_db():
+    '''Opens a new database connection per request.'''        
+    if not hasattr(g, 'db'):
+        g.db = connect_db()
+    return g.db    
+
+@app.teardown_appcontext
+def close_db(error):
+    '''Closes the database connection at the end of request.'''    
+    if hasattr(g, 'db'):
+        g.db.close() 
+
+
 
 @login_manager.user_loader
 def load_user(user_id):
-    cursor = connection.cursor()
+    cursor = get_db().cursor()
     cursor.execute("SELECT * From User WHERE ID = " + str(user_id))
     result = cursor.fetchone()
-    connection.commit()
+    get_db().commit()
     cursor.close()
     if result is None:
         return None
     return User(result["ID"], result["Username"], result["ProfilePic"])
-    
+
 
 @app.route("/")
 def index():
     if flask_login.current_user.is_authenticated:
-        return redirect('/home')
+        return redirect("/home")
     return render_template("landing.html.jinja")
 
 
 @app.route("/register", methods=["GET", "POST"])
 def register():
     if flask_login.current_user.is_authenticated:
-        return redirect('/home')
+        return redirect("/home")
     if request.method == "POST":
         try:
             Username = request.form["username"]
@@ -61,10 +77,10 @@ def register():
             Lastname = request.form["last_name"]
             DOB = request.form["dob"]
             Password = request.form["password"]
-            cursor = connection.cursor()
+            cursor = get_db().cursor()
             sql = "INSERT INTO User (Username, Firstname, Lastname, DOB, Password) VALUES (%s, %s, %s, %s, %s)"
             cursor.execute(sql, (Username, Firstname, Lastname, DOB, Password))
-            connection.commit()
+            get_db().commit()
             cursor.close()
         except IntegrityError as e:
             error_message = "Error: Duplicate username. Please choose another username and try again."
@@ -75,18 +91,18 @@ def register():
 @app.route("/signin", methods=["GET", "POST"])
 def signin():
     if flask_login.current_user.is_authenticated:
-        return redirect('/home')
+        return redirect("/home")
     if request.method == "POST":
         Username = request.form["username"]
         Password = request.form["password"]
-        cursor = connection.cursor()
+        cursor = get_db().cursor()
         sql = f"SELECT * From User WHERE Username = '{Username}'"
         cursor.execute(sql)
         user = cursor.fetchone()
         cursor.close()
-        connection.commit()
+        get_db().commit()
         if user and user["Password"] == Password:
-            user = load_user(user['ID'])
+            user = load_user(user["ID"])
             flask_login.login_user(user)
             return redirect(url_for("home"))
         else:
@@ -95,17 +111,28 @@ def signin():
     return render_template("signin.html.jinja")
 
 
-@app.route("/home")
+@app.route("/home", methods=['POST', 'GET'])
 @flask_login.login_required
 def home():
-    cursor = connection.cursor()
+    cursor = get_db().cursor()
     sql = "SELECT * FROM Posts JOIN `User` ON Posts.User_ID = User.ID ORDER BY `timestamp`"
     cursor.execute(sql)
     posts = cursor.fetchall()
     cursor.close()
     return render_template("home.html.jinja", posts=posts)
 
-
+@app.route('/post', methods=['POST'])
+@flask_login.login_required
+def create_post():
+    description = request.form['Description']
+    image_url = request.form['Image']
+    user_id = flask_login.current_user.id
+    sql = "INSERT INTO Posts (`User_ID`, `Description`, `Image`, `Timestamp`) VALUES (%s, %s, %s, NOW())"
+    cursor = get_db().cursor()
+    cursor.execute(sql, (user_id, description, image_url, ))
+    get_db().commit()
+    cursor.close()
+    return redirect('/home')
 
 if __name__ == "__main__":
     app.run(debug=True)
